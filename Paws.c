31d44c6cdf22adae1438a2b30710c4f12a5ca56f
elliottcable <me@ell.io>
2010-01-25 14:56:13 -0900

    Indiscriminately using struct pointers instead of pass-by-value

--- Paws.c
+++ Paws.c
@@ -48,22 +48,22 @@ struct node
 
 /* ### Method Declarations ### */
 
-struct ll     ll__create();
-void          ll__affix (struct ll, union thing);
-struct node*  ll__at    (struct ll, ll_size);
+struct ll*    ll__create();
+void          ll__affix (struct ll*, struct node*);
+struct node*  ll__at    (struct ll*, ll_size);
 struct /* ll_methods */
 {
-  struct ll     (*create)();
-  void          (*affix) (struct ll, union thing);
-  struct node*  (*at)    (struct ll, ll_size);
+  struct ll*    (*create)();
+  void          (*affix) (struct ll*, struct node*);
+  struct node*  (*at)    (struct ll*, ll_size);
 } const ll = { ll__create, ll__affix, ll__at };
 
 struct node* node__create   (union thing*);
-void         node__attach_to(struct node, struct node*);
+void         node__attach_to(struct node*, struct node*);
 struct /* node_methods */
 {
   struct node* (*create)   (union thing*);
-  void         (*attach_to)(struct node, struct node*);
+  void         (*attach_to)(struct node*, struct node*);
 } const node = { node__create, node__attach_to };
 
 
@@ -72,12 +72,12 @@ struct /* node_methods */
 /* This method initializes a new ll, with no nodes. The `root` is set to a
  * `NULL` pointer.
  */
-struct ll ll__create()
+struct ll* ll__create()
 {
-  struct ll this;
+  struct ll* this = malloc(sizeof(struct ll));
   
-  this.root   = NULL;
-  this.length = 0;
+  this->root   = NULL;
+  this->length = 0;
   
   return this;
 }
@@ -89,23 +89,21 @@ struct ll ll__create()
  * FIXME: Wouldn’t this be ridiculously slow? It has to iterate through the
  * *entire* `ll` before it can affix something.
  */
-void ll__affix(struct ll this, union thing something)
+void ll__affix(struct ll* this, struct node* affixee)
 {
-  struct node* somenode = node.create(thing);
-  
-  node.attach_to( ll.at(this, this.length-1), somenode );
+  node.attach_to( ll.at(this, this->length-1), affixee );
 }
 
 /* This method returns a pointer to the node at a given index in an `ll`.
  * 
  * Takes two arguments, the indexee (`this`), and an integer `index`.
  */
-struct node* ll__at(struct ll this, ll_size index)
+struct node* ll__at(struct ll* this, ll_size index)
 {
   /* This will give us either a `NULL` pointer (if this has no root, i.e. is
    * an empty list), or the first `node` (if this *has* a `root`, i.e. is
    * *not* an empty list). */
-  struct node* last = this.root;
+  struct node* last = this->root;
   
   for(ll_size i = 0; i < index; ++i)
     last = last->next;
@@ -129,9 +127,9 @@ struct node* node__create(union thing* thing)
   return this;
 }
 
-void node__attach_to(struct node this, struct node* to)
+void node__attach_to(struct node* this, struct node* to)
 {
-  to->next = &this;
+  to->next = this;
 }
 
 
@@ -157,9 +155,9 @@ struct list
 
 int main()
 {
-  struct ll root;
+  struct ll* root;
   
-  root = ll.create(NULL);
+  root = ll.create();
   
   return 0;
 }
