ca7b55f15af25d75780bd57128d07efdbc52ca90
elliottcable <me@ell.io>
2010-01-25 20:07:25 -0900

    Re-structuring struct-namespaces

--- Paws.c
+++ Paws.c
@@ -1,19 +1,49 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-union  thing; /* A union representing any core Paws datatype */
+/* Generally speaking, we provide *three* things for every core datatype:
+ * 
+ * - A `struct`, which stores the data relevant to that datatype. These can be
+ *   directly allocated and referenced by their ‘struct tag’:
+ *   
+ *       struct list*  a_list;
+ *       a_list = malloc(sizeof(struct list));
+ *   
+ * - A `const struct` of the same name, capitalized, containing function
+ *   pointers and important values (constants) relevant to that datatype:
+ *   
+ *       a_list = List.create();
+ *   
+ * - A `typedef`’d type (also of the same name), as a *reference* (pointer) to
+ *   items of the original datatype `struct`. This is what most of the core
+ *   API methods return:
+ *   
+ *       list a_list;
+ *       a_list = List.create();
+ *   
+ * It’s important to remember that `list` is a pointer to something, while
+ * `struct list` is that thing itself.
+ * 
+ * For convenience, we also provide a `union thing`, which is a combined type
+ * for a pointer to any core datatype’s `struct`. This is typedef’d as well
+ * (also to `thing`), but directly, not as a pointer (since it is intended to
+ * *contain* pointers).
+ */
+
+union thing; /* A union representing any core Paws datatype */
+typedef union thing thing;
 
 struct ll; /* The data storage system (a linked-list) behind `list` */
 struct node; /* A single node of the linked-list consisting a `ll` */
+typedef struct ll* ll;
+typedef struct node* node;
 
-struct list; /* The actual struct representing Paws’ core datatype,
-              * `infrastructure list`.
-              * NOTE: Most methods return `union thing`s instead! You’ll have
-              *       to manually retreive the `list` from that union. */
+struct list; /* The type behind `infrastructure list`. */
+typedef struct list* list;
 
 union thing
 {
-  struct list*  list;
+  list list;
 };
 
 /* =====
@@ -37,34 +67,34 @@ union thing
 typedef unsigned long int ll_size;
 struct ll
 {
-  struct node*  root; /* A pointer to the first `node` in this `ll` */
+  node          root; /* A pointer to the first `node` in this `ll` */
   ll_size       length; /* The total number of `node`s in this `ll` */
 };
 struct node
 {
-  union thing*  e; /* The `thing` stored at this location in the `ll` */
-  struct node*  next; /* A pointer to the next `node` in the `ll` */
+  thing         e; /* The `thing` stored at this location in the `ll` */
+  node          next; /* A pointer to the next `node` in the `ll` */
 };
 
 /* ### Method Declarations ### */
 
-struct ll*    ll__create();
-void          ll__affix (struct ll*, struct node*);
-struct node*  ll__at    (struct ll*, ll_size);
+ll            ll__create();
+void          ll__affix (ll, node);
+node          ll__at    (ll, ll_size);
 struct /* ll_methods */
 {
-  struct ll*    (*create)();
-  void          (*affix) (struct ll*, struct node*);
-  struct node*  (*at)    (struct ll*, ll_size);
-} const ll = { ll__create, ll__affix, ll__at };
+  ll            (*create)();
+  void          (*affix) (ll, node);
+  node          (*at)    (ll, ll_size);
+} const LL = { ll__create, ll__affix, ll__at };
 
-struct node* node__create   (union thing*);
-void         node__attach_to(struct node*, struct node*);
+node         node__create   (thing);
+void         node__attach_to(node, node);
 struct /* node_methods */
 {
-  struct node* (*create)   (union thing*);
-  void         (*attach_to)(struct node*, struct node*);
-} const node = { node__create, node__attach_to };
+  node         (*create)   (thing);
+  void         (*attach_to)(node, node);
+} const Node = { node__create, node__attach_to };
 
 
 /* ### Method Implementations ### */
@@ -72,9 +102,9 @@ struct /* node_methods */
 /* This method initializes a new ll, with no nodes. The `root` is set to a
  * `NULL` pointer.
  */
-struct ll* ll__create()
+ll ll__create()
 {
-  struct ll* this = malloc(sizeof(struct ll));
+  ll this = malloc(sizeof(struct ll));
   
   this->root   = NULL;
   this->length = 0;
@@ -89,21 +119,21 @@ struct ll* ll__create()
  * FIXME: Wouldn’t this be ridiculously slow? It has to iterate through the
  * *entire* `ll` before it can affix something.
  */
-void ll__affix(struct ll* this, struct node* affixee)
+void ll__affix(ll this, node affixee)
 {
-  node.attach_to( ll.at(this, this->length-1), affixee );
+  Node.attach_to( LL.at(this, this->length-1), affixee );
 }
 
 /* This method returns a pointer to the node at a given index in an `ll`.
  * 
  * Takes two arguments, the indexee (`this`), and an integer `index`.
  */
-struct node* ll__at(struct ll* this, ll_size index)
+node ll__at(ll this, ll_size index)
 {
   /* This will give us either a `NULL` pointer (if this has no root, i.e. is
    * an empty list), or the first `node` (if this *has* a `root`, i.e. is
    * *not* an empty list). */
-  struct node* last = this->root;
+  node last = this->root;
   
   for(ll_size i = 0; i < index; ++i)
     last = last->next;
@@ -116,10 +146,10 @@ struct node* ll__at(struct ll* this, ll_size index)
  * 
  * Expects a `thing` as an argument, to be stored on this `node` as `e`.
  */
-struct node* node__create(union thing* thing)
+node node__create(thing thing)
 {
   /* LEAK: Well, what exactly can we do? It’s not like we have a GC yet… */
-  struct node* this = malloc(sizeof(struct node));
+  node this = malloc(sizeof(struct node));
   
   this->e    = thing;
   this->next = NULL;
@@ -127,7 +157,7 @@ struct node* node__create(union thing* thing)
   return this;
 }
 
-void node__attach_to(struct node* this, struct node* to)
+void node__attach_to(node this, node to)
 {
   to->next = this;
 }
@@ -145,7 +175,7 @@ void node__attach_to(struct node* this, struct node* to)
  */
 struct list
 {
-  struct ll*  naughty; /* The ll behind this `list` */
+  ll naughty; /* The ll behind this `list` */
 };
 
 
@@ -155,9 +185,9 @@ struct list
 
 int main()
 {
-  struct ll* root;
+  ll root;
   
-  root = ll.create();
+  root = LL.create();
   
   return 0;
 }
