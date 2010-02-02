2f4cc7fe105463dcaf62bbc9117728ee0b5d4cbf
elliottcable <me@ell.io>
2010-02-01 17:30:41 -0900

    Uh, I need to figure out a good way to test

---
+++ Source/Paws.o/Core/ll.tests.c
@@ -0,0 +1,49 @@
+#include "ll.c"
+
+#include <stdbool.h>
+
+bool ll__test_create  (void);
+bool ll__test_anterior_insert   (void);
+bool ll__test_posterior_insert  (void);
+bool ll__test_prefix  (void);
+bool ll__test_affix   (void);
+bool ll__test_at      (void);
+
+static const int LL_tests_length = 6;
+bool (*const LL_tests[])(void) = {
+  ll__test_create,
+  ll__test_anterior_insert,
+  ll__test_posterior_insert,
+  ll__test_prefix,
+  ll__test_affix,
+  ll__test_at
+};
+
+bool ll__test_create(void) {
+  return true;
+}
+
+bool ll__test_anterior_insert(void) {
+  return true;
+}
+
+bool ll__test_posterior_insert(void) {
+  return true;
+}
+
+bool ll__test_prefix(void) {
+  return true;
+}
+
+bool ll__test_affix(void) {
+  return true;
+}
+
+bool ll__test_at(void) {
+  return true;
+}
+
+int main(void) {
+  
+  return 0;
+}
