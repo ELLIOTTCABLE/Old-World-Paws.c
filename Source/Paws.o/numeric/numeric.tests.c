1a89f51c4a965259586718e67c40ad8123d77152
elliottcable <me@ell.io>
2010-04-29 05:36:33 -0800

    (tests minor) Copying the `string` tests to `numeric`

--- Source/Paws.o/numeric/numeric.tests.c
+++ Source/Paws.o/numeric/numeric.tests.c
@@ -4,7 +4,35 @@
 # include "Cest.h"
 #endif
 
+CEST(Numeric, create) {
+  numeric   a_numeric;
+  list      a_naughty;
+  
+  a_numeric = Numeric.create(42);
+  ASSERT( a_numeric->native == 42 );
+  
+  a_naughty = LL.at(a_numeric->content, 0)->thing->pointer.list;
+  ASSERT( LL.at(a_numeric->content, 0)->thing->isa == LIST );
+  ASSERT( LL.at(a_numeric->content, 0)->thing->pointer.list == a_naughty );
+  
+  return true;
+}
 
-CEST(Numeric, create) { return false; }
-CEST(numeric, to_thing) { return false; }
-CEST(numeric, native) { return false; }
+CEST(numeric, to_thing) {
+  numeric   a_numeric = Numeric.create(42);
+  thing     a_thing;
+  
+  a_thing = Numeric.to_thing(a_numeric);
+  ASSERT( a_thing->isa             == NUMERIC   );
+  ASSERT( a_thing->pointer.numeric == a_numeric );
+  
+  return true;
+}
+
+CEST(numeric, native) {
+  numeric   a_numeric = Numeric.create(42);
+  
+  ASSERT( Numeric.native(a_numeric) == 42 );
+  
+  return true;
+}
