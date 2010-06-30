9e408b673730f5d8ded6bd3325444f5d61114505
elliottcable <me@ell.io>
2010-06-30 02:40:12 -0800

    Added a legacy `Paws.h` header; just to be safe, for people who assume everything uses the ‘header’ system.

---
+++ Source/Paws.o/Paws.h
@@ -0,0 +1,15 @@
+/* This file is deprecated, and is simply left in for lazy people. Instead of using this file, you should use the
+   following includes:
+   
+       #define DECLARATIONS
+       # include "Paws.o/Paws.c"
+       #undef  DECLARATIONS */
+
+#if !defined(DECLARATIONS)
+# define PAWS_H__BEHEST
+# define DECLARATIONS
+#endif
+#   include "Paws.c"
+#if defined(PAWS_H__BEHEST)
+# undef DECLARATIONS
+#endif
